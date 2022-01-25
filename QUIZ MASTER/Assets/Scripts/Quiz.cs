0df38b321622af877088be9a8034fa381120aeac
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-25 13:52:20 +0300

    QUİZ MASTER 1

---
+++ QUIZ MASTER/Assets/Scripts/Quiz.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField]TextMeshProUGUI questionText;
+    [SerializeField]QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    
+    void Start()
+    {
+        
+       
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < 4; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+        
+    }
+   
+}
